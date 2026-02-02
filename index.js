fa717bde42b1ca4d9bdcc984bb1cc59cd973f909
Shikha <ramrakhyani.shikha@gmail.com>
2026-02-02 20:40:43 +0530

    updated index.js

--- index.js
+++ index.js
@@ -335,7 +335,8 @@ app.delete('/hotels/:hotelId', async (req,res) =>{
         res.status(500).json({error:"Failed to delete hotel."})
     }
 } );
-const PORT = 3000;
-app.listen(PORT,() => {
-    console.log(`Server is running on ${PORT}`);
-})
+// const PORT = 3000;
+// app.listen(PORT,() => {
+//     console.log(`Server is running on ${PORT}`);
+// })
+module.exports = app;
